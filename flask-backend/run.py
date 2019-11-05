52090ccea56ac97e5a64f5f666b9a62a058aaa29
tjdinhngo <ngo3@umbc.edu>
2019-11-05 16:28:25 -0500

    ec2 fix

--- flask-backend/run.py
+++ flask-backend/run.py
@@ -7,6 +7,4 @@ app = flask.Flask("__main__")
 def hello_world():
     return flask.render_template("index.html", token= "React and Flask!")
 
-app.run(debug=True)
-#if __name__ == '__main__':
-#    app.run(host='0.0.0.0', port='3000')
+app.run(host='0.0.0.0', port='3000')
