4c4205543195b8b695db5e0ec5cfff3880cb455a
Keegan Scott <ezeek.scott@gmail.com>
2019-11-10 04:16:28 -0500

    current progress

---
+++ flask_backend/.flaskenv
@@ -0,0 +1,3 @@
+FLASK_APP=flask_backend.py
+FLASK_ENV=development
+OPENBALT_URL=data.baltimorecity.gov/resource/wsfq-mvij.json?crimedate=2019-09-14
---
+++ flask_backend/static/react/precache-manifest.521340a7e20ff8af86ce1b23275813fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba233c3d80d6ab474e0a4ae53d09e59d",
+    "url": "static/react/../../templates/index.html"
+  },
+  {
+    "revision": "6378da9e0a751e36566d",
+    "url": "static/react/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "bc16bc113efbc246507e",
+    "url": "static/react/js/2.ba4a26ae.chunk.js"
+  },
+  {
+    "revision": "6378da9e0a751e36566d",
+    "url": "static/react/js/main.b3bf14f6.chunk.js"
+  },
+  {
+    "revision": "b3e4df5d829553de4fa9",
+    "url": "static/react/js/runtime-main.35355ffc.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "static/react/media/logo.25bf045c.svg"
+  }
+]);
