65009cc0a4aad96cb18a8c26dd348f5488392589
Keegan Scott <ezeek.scott@gmail.com>
2019-11-11 11:47:35 -0500

    yea

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import {BarChart} from 'react-easy-chart';
 import Chart from './Chart'
+import {XYPlot, XAxis, YAxis, VerticalGridLines, HorizontalGridLines, LineSeries} from 'react-vis';
 
 var origin = window.location.origin;
 var url = origin + '/api/crimes'
@@ -48,30 +49,48 @@ class GetCrimes extends React.Component {
     } else if (!isLoaded) {
       return <div>Loading...</div>;
     } else {
+      var xydata = [];
+      var lastdate = "0";
+      var total_crimes = 0;
+      for (var i = 0; i < data.length; i++) {
+        if (lastdate == "0") {
+          lastdate = data[i].crimedate;
+        } else if (data[i].crimedate != lastdate) {
+          var newdata = {x: lastdate, y: total_crimes};
+          xydata.push(newdata);
+
+          lastdate = data[i].crimedate;
+          total_crimes = 0;
+        }
+
+        total_crimes += 1;
+      }
+
       return (
         <div className="App">
-        <Chart data={data}/>
-        {/*
-        <BarChart
-        data={[
-          {x: 'A', y: 20},
-          {x: 'B', y: 30},
-          {x: 'C', y: 40},
-          {x: 'D', y: 20},
-          {x: 'E', y: 40},
-          {x: 'F', y: 25},
-          {x: 'G', y: 5}
-        ]}
-        />
-        <p>My Token = {window.token}</p>
+        <XYPlot
+            xType="ordinal"
+            width={1000}
+            height={500}>
+            <VerticalGridLines />
+            <HorizontalGridLines />
+            <XAxis title="Period of time" />
+            <YAxis title="Number of Crimes" />
+                <LineSeries
+                    data={xydata}
+                    color="violet"
+                    strokeWidth="3"
+                    // fill="none"
+                    // style={{stroke: 'violet', strokeWidth: 3}}
+                    />
+        </XYPlot>
         <ul>
             {data.map(data => (
               <li key={data.post}>
-                {data.crimedate} {data.crimetime}
+                {data.crimedate} {data.crimetime} {data.description} {data.weapon}
               </li>
             ))}
           </ul>
-          */}
         </div>
       )
     }
