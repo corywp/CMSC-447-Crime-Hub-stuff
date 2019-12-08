9c51c8a6830d22cef415f8c0295616fa878305d0
tjdinhngo <ngo3@umbc.edu>
2019-12-07 21:30:45 -0500

    Heatmap data

--- react_frontend/src/LineGraph.js
+++ react_frontend/src/LineGraph.js
@@ -1,24 +1,62 @@
 import React from "react";
 import { LineChart } from "react-d3-components";
 class LineGraph extends React.Component{
-    constructor(props)
-    {
-      super(props);
-    }
-    makeGraph() {
-      var data = [
-        {
-        label: 'somethingA',
-        values: [{x: 0, y: 2}, {x: 1.3, y: 5}, {x: 3, y: 6}, {x: 3.5, y: 6.5}, {x: 4, y: 6}, {x: 4.5, y: 6}, {x: 5, y: 7}, {x: 5.5, y: 8}]
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      data: [],
+      graphData: []
+      }
+      this.makeGraph = this.makeGraph.bind(this);
+  }
+  componentDidMount() {
+    fetch("http://127.0.0.1:3000/api/crimes")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            data: result
+          });
         },
-        {
-        label: 'somethingB',
-        values: [{x: 0, y: 3}, {x: 1.3, y: 4}, {x: 3, y: 7}, {x: 3.5, y: 8}, {x: 4, y: 7}, {x: 4.5, y: 7}, {x: 5, y: 7.8}, {x: 5.5, y: 9}]
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
         }
-    ];
-      return (data);
-    }
-  render(){
+      )
+  }
+
+      makeGraph() {
+        console.log(this.state.graphData);
+        var data = {
+          label: 'Weapon Distribution',
+          values: this.state.graphData
+        };
+        return (data);
+      }
+  render() {
+    const { error, isLoaded, data } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+          var xydata = [];
+          var unique_weapon_types = "";
+          var crime_count_by_weapon_type = 0;
+          for (var i = 0; i < data.neighborhood_names.length; i++) {
+          var newdata = {x: data.neighborhood_names[i], y: data.overall_crime_count_by_district[data.neighborhood_names[i]]};
+          xydata.push(newdata);
+      }
+      this.state.graphData = [... xydata];
   return (
     <LineChart
       data={this.makeGraph()}
@@ -27,6 +65,7 @@ class LineGraph extends React.Component{
       margin={{ top: 10, bottom: 50, left: 50, right: 10 }}
     />
   );
+}
 }
 }
 export default LineGraph;
--- react_frontend/src/Map.js
+++ react_frontend/src/Map.js
@@ -2,27 +2,74 @@ import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
-
+var origin = window.location.origin;
+var url = origin + '/api/crimes'
+var g = [
+  'rgba(0,255,255,0)','rgba(0,255,255,1)','rgba(0,191,255,1)','rgba(0,127,255,1)',
+	'rgba(0,63,255,1)','rgba(0,0,223,1)','rgba(0,0,191,1)','rgba(0,0,159,1)',
+	'rgba(0,0,127,1)','rgba(63,0,91,1)','rgba(191,0,31,1)','rgba(255,0,0,1)',
+]
 class Map extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      data: []
+    };
+  }
   static defaultProps = {
     center: {
-      lat: 39.2201,
-      lng: -76.6122
+      lat: 39.2858,
+      lng: -76.6131
     },
     zoom: 13,
     data: {
   positions: [
-    {lat: 39.2201, lng: -76.6122},
-    {lat: 39.2204, lng: -76.6122},
   ],
   options: {
-    radius: 20,
-    opacity: 0.6,
+    radius: 50,
+    gradient: g,
   }
   }
 }
-
+componentDidMount() {
+  fetch("http://127.0.0.1:3000/api/crimes")
+    .then(res => res.json())
+    .then(
+      (result) => {
+        this.setState({
+          isLoaded: true,
+          data: result
+        });
+      },
+      // Note: it's important to handle errors here
+      // instead of a catch() block so that we don't swallow
+      // exceptions from actual bugs in components.
+      (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
+      }
+    )
+}
   render() {
+    const { error, isLoaded, data } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+          var xydata = [];
+          var unique_weapon_types = "";
+          var crime_count_by_weapon_type = 0;
+          for (var i = 0; i < data.crimes.length; i++) {
+          var newdata = {lat: data.crimes[i]["latitude"], lng: data.crimes[i]["longitude"]};
+          xydata.push(newdata);
+          }
+        this.props.data["positions"] = [... xydata];
+        console.log(this.props.data);
     return (
       // Important! Always set the container height explicitly
       <div style={{ height: '100vh', width: '100%' }}>
@@ -38,5 +85,5 @@ class Map extends Component {
     );
   }
 }
-
+}
 export default Map;
