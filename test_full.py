357d6f7159440843929f9477ca1dd6f8651e64a9
Keegan Scott <ezeek.scott@gmail.com>
2019-12-12 21:49:15 -0500

    updated black box test

--- test_full.py
+++ test_full.py
@@ -1,16 +1,22 @@
 import unittest
 import subprocess
+import signal
+import os
+import json
 import requests
 
 class TestFull(unittest.TestCase):
     def test(self):
         # run server as a subprocess
-        proc = subprocess.Popen(['cd', 'flask_backend', '&', 'C:\\Users\\Keegan\\AppData\\Local\\Programs\\Python\\Python37-32\\python.exe', '-m', 'flask', 'run', '-h', '127.0.0.1', '-p', '3000'], shell=True, stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
-        
-        try:
-            response = requests.get('http://localhost:3000/api/crimes')
-            self.assertEqual(response.status_code, 200)
-            
-        finally:
-            proc.terminate()
-            print('== subprocess exited with rc =', proc.returncode)
+        proc = subprocess.Popen(['cd', 'flask_backend', '&', 'python', '-m', 'flask', 'run', '-h', '127.0.0.1', '-p', '3000'], shell=True, stdout=subprocess.PIPE)
+
+        response = requests.get('http://localhost:3000/api/crimes')
+        self.assertEqual(response.status_code, 200)
+
+        data = json.loads(response.content)
+
+        self.assertTrue('crimes' in data)
+        self.assertNotEqual(len(data['crimes']), 0)
+
+        proc.kill()
+        proc.terminate()
